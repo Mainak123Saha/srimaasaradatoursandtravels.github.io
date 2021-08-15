7d9745bde792a5e0effe9bdf7226f5c39c395bf2
Mainak <mainak123saha@gmail.com>
2021-08-15 22:51:34 +0530

    Changes in filter styles and logic

--- assets/scripts/packages.js
+++ assets/scripts/packages.js
@@ -19,3 +19,39 @@ item.forEach(elem => {
         indicator(item[active_tab]);
     });
 });
+
+
+
+
+
+
+const date_range = document.querySelector('.packages .packageHead .dateRange input');
+const date_range_text = document.querySelector('.packages .packageHead .dateRange span');
+date_range.addEventListener('input', ()=>{
+    switch (date_range.value) {
+        case "0":
+            date_range_text.innerHTML = "Random";
+            break;
+        case "1":
+            date_range_text.innerHTML = "3 Nights 4 Days";
+            break;
+        case "2":
+            date_range_text.innerHTML = "4 Nights 5 Days";
+            break;
+        case "3":
+            date_range_text.innerHTML = "5 Nights 6 Days";
+            break;
+        case "4":
+            date_range_text.innerHTML = "6 Nights 7 Days";
+            break;
+        case "5":
+            date_range_text.innerHTML = "7 Nights 8 Days";
+            break;
+        case "6":
+            date_range_text.innerHTML = "8 Nights 9 Days";
+            break;
+        case "7":
+            date_range_text.innerHTML = "9 Nights 10 Days";
+            break;
+    }
+})
