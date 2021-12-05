95c17bb9d65320f5b5c2ce78f74bbb9abae3c4f4
Mainak <mainak123saha@gmail.com>
2021-12-05 12:12:16 +0530

    removed all

--- assets/scripts/index.js
+++
@@ -1,7 +0,0 @@
-// Firebase
-// db.collection('users').get().then((snapshot)=>{
-//     console.log(snapshot.docs);
-//     snapshot.docs.forEach(doc =>{
-//         console.log(doc.data());
-//     })
-// });
